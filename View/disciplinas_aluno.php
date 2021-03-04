9e96e0aa294496c9cf2ac36dbd97969fd6444234
Rafael Myauti <myauti.rafa@outlook.com>
2021-03-04 18:51:07 -0300

    Alteração no botão

--- View/disciplinas_aluno.php
+++ View/disciplinas_aluno.php
@@ -58,7 +58,7 @@ $rs = $conec->query($sql);
     <div class="h-75 container d-flex justify-content-around  mt-5">
       <div class="d-flex p-2">
         <?php while ($obj = $rs->fetch_object()) { ?>
-          <button onclick="window.location.href = 'aluno.php'" type="button" class="mb-4 rounded btn btn-outline-primary btn-lg ml-xxl-3"><?php echo $obj->nome_disciplina . " " . "-" . " " . $obj->aluno_disciplina_vinculada ?></button>
+          <button onclick="window.location.href = 'aluno.php'" type="button" class="mb-4 rounded btn btn-outline-primary btn-lg ml-xxl-3"><?php echo $obj->nome_disciplina?></button>
         <?php } ?>
       </div>
     </div>
