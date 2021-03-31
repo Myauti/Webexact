9ad4d3580f157bf32223bf532ef75b57033002fc
Rafael Myauti <myauti.rafa@outlook.com>
2021-03-31 17:05:59 -0400

    Adicionado: Retirar string de um array

--- Controller/logar.php
+++ Controller/logar.php
@@ -13,11 +13,30 @@
         $sql="SELECT * FROM usuarios WHERE cpf = '$usuario' && senha = '$senha' LIMIT 1";
         $result = mysqli_query($conec, $sql);
         $resultado = mysqli_fetch_assoc($result);
+        $id_usuario = $resultado['id_usuario'];
 
+       $consulta = "SELECT pd.disciplina_vinculada FROM professor_disciplina as pd 
+        INNER JOIN professores as p ON pd.professor_vinculado = p.id_professor 
+        INNER JOIN usuarios as u ON p.usuario_professor = u.id_usuario 
+        WHERE u.id_usuario = $id_usuario";
         
+        $varConsulta = $conec->query($consulta);
+
+        $datas = array();
+        $p=0;
+        
+        while($obj = $varConsulta->fetch_object()){
+            $datas[$p] = $obj->disciplina_vinculada;
+            $p++;
+        }
+        
+        
+
         $_SESSION['id_user'] = $resultado['id_usuario'];
         $_SESSION['usuario'] = $usuario;
         $_SESSION['senha'] = $senha;
+        $_SESSION['disciplinas'] = implode(",", $datas);
+        $_SESSION['vetor_disciplinas'] = $datas;
    
         if(empty($resultado)){
             $_SESSION['loginErro'] = "Usuário ou senha Inválido";
@@ -30,7 +49,7 @@
                 header("Location: ../View/disciplinas.php");
             }
             elseif($_SESSION['usuarioId'] == "2"){
-                header("Location: ../View/aluno.html");
+                header("Location: ../View/disciplinas_aluno.php");
             }
             elseif($_SESSION['usuarioId'] == "1"){
                 header("Location: ../View/adm.php");
