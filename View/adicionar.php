e0043123181c6337c4b11a97a7bd1c823f69c568
Rafael Myauti <myauti.rafa@outlook.com>
2021-04-01 09:28:42 -0400

    Corrigido o caminho do home no header

--- View/adicionar.php
+++ View/adicionar.php
@@ -22,7 +22,7 @@
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
           <ul class="navbar-nav">
             <li class="nav-item active">
-              <a class="nav-link" href="index.html">Home<span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="../index.html">Home<span class="sr-only">(current)</span></a>
             </li>
           </ul>
         </div>
